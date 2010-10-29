cdc217a1aeb0dce38c12b30993e8b36d20a3fce5
P. Mark Anderson <mark@bordertownlabs.com>
2010-10-29 11:23:34 -0700

    Minor

--- Classes/ProductInfoAppDelegate.h
+++ Classes/ProductInfoAppDelegate.h
@@ -3,7 +3,7 @@
 //  ProductInfo
 //
 //  Created by P. Mark Anderson on 10/26/10.
-//  Copyright 2010 Digimarc Corporation, USA. All rights reserved.
+//  Copyright 2010 Bordertown Labs, LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
